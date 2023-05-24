ac566e035cbc8e6204faa611e7ab63c762a20412
Stanislav Ksenzov <123361889+daxx0n@users.noreply.github.com>
2023-05-24 13:03:20 +0000

    adding view_Author

--- src/directories/views.py
+++ src/directories/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse
 
 # Create your views here.
+
+def view_Author(request, pk):
+    print (pk)
+    author = models.Author.objects.get(pk=int(pk))
+    html = f"Author PK:{author.pk} Author name {author.name}"
+    return HttpResponse(html)
+    
